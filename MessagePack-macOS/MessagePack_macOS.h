1682869e88890136c927dc0f5721a76da51276e5
Langxxx <316230604@qq.com>
2017-11-20 11:47:00 +0800

    Support macOS

---
+++ MessagePack-macOS/MessagePack_macOS.h
@@ -0,0 +1,19 @@
+//
+//  MessagePack_macOS.h
+//  MessagePack-macOS
+//
+//  Created by lang on 20/11/2017.
+//  Copyright © 2017 Beary Innovative. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MessagePack_macOS.
+FOUNDATION_EXPORT double MessagePack_macOSVersionNumber;
+
+//! Project version string for MessagePack_macOS.
+FOUNDATION_EXPORT const unsigned char MessagePack_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MessagePack_macOS/PublicHeader.h>
+
+
